595b99ae405d8d5ef83e850afd34a653cc9ff403
Bima Yulia Putra <bimamojt@gmail.com>
2015-10-01 14:02:00 +0700

    Tugas Warna

---
+++ warna/warna/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Aqua Then
+            Me.BackColor = Color.Lime
+        Else
+            Me.BackColor = Color.Aqua
+        End If
+    End Sub
+End Class
